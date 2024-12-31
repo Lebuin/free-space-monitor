2f6ddaf77cc8aed9da768303bbba37abef102706
Seppe Lenders <seppe.lenders@gmail.com>
2024-12-31 12:53:56 +0100

    switch to healthchecks.io

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-message_sent=false
-
-send_message () {
-  message=$1
-  wget -q -O - --header "Content-Type: application/json" --post-data "{\"token\": \"$PUSHOVER_API_TOKEN\", \"user\": \"$PUSHOVER_USER_KEY\", \"message\": \"$message\"}"  https://api.pushover.net/1/messages.json
+# Status 0: sufficient disk space
+# Status 1: disk space is low
+send_status () {
+  status=$1
+  wget -q -O - $HEALTHCHECK_URL/$status
   echo
 }
 
@@ -16,12 +16,12 @@ while true; do
   echo "Available: ${available} MB"
 
   if (( $available > $MIN_FREE_SPACE)); then
-    message_sent=false
-  elif ! $message_sent; then
-    echo "Sending message to Pushover"
-    send_message "$PUSHOVER_MESSAGE"
-    message_sent=true
+    status=0
+  else
+    status=1
   fi
+
+  send_status $status;
 
   sleep $INTERVAL
 done
